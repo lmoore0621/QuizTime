50b54e9d4b823d203da34ef85927a68479f15e59
Leon Moore <lmoore0621@gmail.com>
2017-04-14 03:22:40 -0400

    Edit Answers

--- QuizProj/Scripts/takeQuiz.js
+++ QuizProj/Scripts/takeQuiz.js
@@ -12,9 +12,27 @@
         $.getJSON("/api/QuestionApi/CorrectAnswer", answerSubmission)
         .done(function (data) {
             if (data.IsCorrect) {
-                alert("Yes! Thats Correct!");
+                var ans
+                //document.getElementById("answer-result").innerHTML = "Right: Yeah! You got it.";
+                document.getElementById("answer-result").innerHTML =
+                    '<div class="panel panel-primary">' +
+                       '<div class="panel-heading">' +
+                           'Right' +
+                       '</div>' +
+                       '<div class="panel-body right-answer">' +
+                           'Yeah! You got it.' +
+                       '</div>' +
+                     '</div>';
             } else {
-                alert("Sorry! That is incorrect");
+                document.getElementById("answer-result").innerHTML =
+                    '<div class="panel panel-danger">' +
+                          '<div class="panel-heading">' +
+                              'Wrong' +
+                          '</div>' +
+                          '<div class="panel-body wrong-answer">' +
+                              'Sorry, you are a loser.' +
+                          '</div>' +
+                        '</div>';
             }
         })
         .fail(function (jqxhr, textStatus, error) {
@@ -25,18 +43,31 @@
     nextQuestionBtn.addEventListener('click', function () {
         $.getJSON("/api/QuestionApi", function (data) {
             var questionDiv = document.createElement('div');
+
             uploadedQuiz.innerHTML = "";
             currentQuestionId = data.QuestionId;
-            uploadedQuiz.id = currentQuestionId;
-            uploadedQuiz.appendChild(questionDiv);
 
+            //uploadedQuiz.id = currentQuestionId;
+            uploadedQuiz.appendChild(questionDiv);
             questionDiv.innerText = data.Content;
+
             data.Answers.forEach(function (val) {
                 var answerDiv = document.createElement('div');
+
                 answerDiv.innerText = val.Content;
                 answerDiv.id = val.AnswerId;
                 answerDiv.addEventListener('click', clickAction);
                 uploadedQuiz.appendChild(answerDiv);
+                answerDiv.className = 'answerBox';
+                answerDiv.addEventListener("mouseenter", function (event) {
+                    event.target.style.color = "green";
+                });
+                answerDiv.addEventListener("mouseout", function (event) {
+                    event.target.style.color = "black";
+                });
+
+
+                uploadedQuiz.className = 'questionText';
             });
         });
     });
