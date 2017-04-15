f88ebcf38fdfcabe3890e8361f838cffa8d326e7
Leon Moore <lmoore0621@gmail.com>
2017-04-15 16:24:40 -0400

    Edit resultBox and count Errors

--- QuizProj/Scripts/takeQuiz.js
+++ QuizProj/Scripts/takeQuiz.js
@@ -1,21 +1,28 @@
 ﻿$(document).ready(function () {
     var uploadedQuiz = document.getElementById("uploadedQuiz"),
     nextQuestionBtn = document.getElementById("nextQuestionBtn"),
+    clickCount = 0,
     currentQuestionId;
+
+    var countClicks = function() {
+            clickCount++
+    };    
 
     var clickAction = function clickAction(action) {
         var answerSubmission = {
             questionId: currentQuestionId,
             answerId: action.currentTarget.id
         };
+        countClicks();
 
         $.getJSON("/api/QuestionApi/CorrectAnswer", answerSubmission)
         .done(function (data) {
-            if (data.IsCorrect) {
-                var chances = 0;
-                var answer = 0;
-                var cream = 5
 
+            var correct = data.IsCorrect;
+            var incorrect = !data.IsCorrect;
+            var chances = 3;
+
+            if (correct) {
                 document.getElementById("answer-result").innerHTML =
                     '<div class="panel panel-primary">' +
                        '<div class="panel-heading">' +
@@ -25,15 +32,35 @@
                            'Yeah! You got it.' +
                        '</div>' +
                      '</div>';
-            } else {
-              
+            } 
+            else if (incorrect && clickCount < 3) {
                 document.getElementById("answer-result").innerHTML =
                     '<div class="panel panel-danger">' +
                           '<div class="panel-heading">' +
                               'Incorrect' +
                           '</div>' +
                           '<div class="panel-body wrong-answer">' +
-                              'Sorry, you are a loser.' +
+                              'You were incorrect ' +
+                              clickCount + ' time(s), ' +
+                              'You have ' + (chances - clickCount) +
+                              ' chance(s) left.' +
+                          '</div>' +
+                       '</div>';
+            }
+            else {
+                var answerBoxes = document.getElementsByClassName('answerBox');
+                for (var i = 0; i < answerBoxes.length; i++)
+                {
+                    answerBoxes[i].removeEventListener('click', clickAction);
+                }
+
+                document.getElementById("answer-result").innerHTML =
+                    '<div class="panel panel-danger gameOver">' +
+                          '<div class="panel-heading">' +
+                              'Incorrect' +
+                          '</div>' +
+                          '<div class="panel-body wrong-answer">' +
+                              'Sorry, you got 3 incorrect. Go to the next Question' +
                           '</div>' +
                        '</div>';
             }
@@ -43,9 +70,46 @@
         });
     }
 
+    $.getJSON("/api/QuestionApi", function (data) {
+        clickCount = 0;
+
+        var questionDiv = document.createElement('div');
+        document.getElementById('answer-result').innerHTML = '';
+
+        uploadedQuiz.innerHTML = "";
+        currentQuestionId = data.QuestionId;
+
+        uploadedQuiz.appendChild(questionDiv);
+        questionDiv.innerText = data.Content;
+
+        data.Answers.forEach(function (val) {
+            var answerDiv = document.createElement('div');
+
+            answerDiv.innerText = val.Content;
+            answerDiv.id = val.AnswerId;
+            answerDiv.addEventListener('click', clickAction);
+            uploadedQuiz.appendChild(answerDiv);
+
+            answerDiv.className = 'answerBox';
+            answerDiv.addEventListener("mouseenter", function (event) {
+                event.target.style.color = "green";
+            });
+            answerDiv.addEventListener("mouseout", function (event) {
+                event.target.style.color = "black";
+            });
+
+
+            uploadedQuiz.className = 'questionText';
+        });
+    });
+
     nextQuestionBtn.addEventListener('click', function () {
         $.getJSON("/api/QuestionApi", function (data) {
+            clickCount = 0;
+            
+
             var questionDiv = document.createElement('div');
+            document.getElementById('answer-result').innerHTML = '';
 
             uploadedQuiz.innerHTML = "";
             currentQuestionId = data.QuestionId;
@@ -60,6 +124,7 @@
                 answerDiv.id = val.AnswerId;
                 answerDiv.addEventListener('click', clickAction);
                 uploadedQuiz.appendChild(answerDiv);
+
                 answerDiv.className = 'answerBox';
                 answerDiv.addEventListener("mouseenter", function (event) {
                     event.target.style.color = "green";
